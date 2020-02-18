b658678d85fa20df3fa92f51e9caa079f84e51c1
Edgar Gonzalez <edgar_gm6@hotmail.com>
2020-02-17 18:59:53 -0600

    Avances en Tiempo

--- tiempo.js
+++ tiempo.js
@@ -1,15 +1,30 @@
 class Tiempo {
-    /**
-     * 
-     * @param {number} hora 
-     * @param {number} minutos 
-     * @param {string} periodo 
-     */
-  constructor(hora, minutos, periodo) {
-    this.hora = hora;
-    this.minutos = minutos;
-    this.periodo = periodo;
+  /**
+   *
+   * @param {number} hora
+   * @param {number} minutos
+   * @param {string} periodo
+   */
+  constructor() {
+    this.hora = new Date();
+    this.minutos = new Date();
   }
-  getFormato12() {}
-  getFormato24() {}
+  getFormato12() {
+    let periodo = ["AM", "PM"];
+    if (this.hora.getHours() < 12) {
+      periodo = [0];
+    } else {
+      periodo = [1];
+    }
+    Hora = this.hora.getHours() % 12;
+    if (Hora == 0) {
+      Hora = 12;
+    }
+  }
+  getFormato24() {
+    return `${this.hora.getHours()}:${this.minutos.getMinutes()}`;
+  }
 }
+
+let h = new Tiempo();
+console.log(h.getFormato24());
